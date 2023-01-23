6de837bbd3cf08c45d8dd58178d82271e184be83
Adhyan Negi <adineg854@gmail.com>
2023-01-22 22:09:50 -0500

    solved most of the problems

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let len = text.length;
+    if (len === 1){
+        return text;
+    }
+    let reverse = "";
+    for (let i = (len - 1); i >= 0; i++){
+        reverse += text.charAt(i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array1, ...args) {
+    const ans = [];
+    array1.forEach((item) => {
+        if (!args.includes(item)) {
+            ans.push(item);
+        }
+    })
+    return ans;
 };
 
 // Do not edit below this line
